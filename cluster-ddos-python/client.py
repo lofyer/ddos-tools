72991995d75becb0c71083bff9047f385ccad4e5
lofyer <lofyer@gmail.com>
2014-08-19 22:24:19 +0800

    更新python

--- cluster-ddos-python/client.py
+++ cluster-ddos-python/client.py
@@ -2,18 +2,19 @@
 import subprocess
 import socket
 
+print "Client begin"
 # get client ip
 a=subprocess.Popen("curl ifconfig.me",stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
 postip=a.stdout.read()
 
-address = ('lofyer.org', 30000)
+address = ('localhost', 30000)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(address)
-
-data = s.recv(1024)
-print "Received data is ", data
-
-print postip
-s.send(postip)
-
-s.close()
+try:
+	s.connect(address)
+	data = s.recv(1024)
+	print "Received data is ", data
+	print postip
+	s.send(postip)
+	s.close()
+except Exception, e:
+	print "error"

